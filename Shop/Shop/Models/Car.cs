5dc786a8cba7d41abb3813d3f725d9af2d293005
AlexSkorupsky <alex.skorupskyy@gmail.com>
2018-12-03 17:19:31 +0200

    added base mocks

--- Shop/Shop/Models/Car.cs
+++ Shop/Shop/Models/Car.cs
@@ -9,12 +9,12 @@ namespace Shop.Data.Models
         public int CarId { get; set; }
         public string Name { get; set; }
         public string ShortDescription { get; set; }
-        public string LongDecription { get; set; }
+        public string LongDescription { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
         public string ImageThumbnailUrl { get; set; }
-        public bool IsPreferedCar { get; set; }
-        public int InStock { get; set; }
+        public bool IsPreferredCar { get; set; }
+        public bool InStock { get; set; }
         public int CategoryId { get; set; }
         public virtual Category Category { get; set; }
     }
