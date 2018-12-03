41b2106edfec769007c8a842839e718274e2d6f7
AlexSkorupsky <alex.skorupskyy@gmail.com>
2018-12-03 14:13:09 +0200

    added base repositories

---
+++ Shop/Shop/Interfaces/ICarRepository.cs
@@ -0,0 +1,17 @@
+﻿using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Interfaces
+{
+    interface ICarRepository
+    {
+        IEnumerable<Car> Cars { get; }
+
+        IEnumerable<Car> PreferredCars { get; }
+
+        Car GetCarById(int CarId);
+    }
+}
