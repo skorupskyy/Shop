e53145466b3cfeb100c9c19f1014882e3ed60c47
Oleg Zhuk <2ndriy@gmail.com>
2018-12-13 11:53:35 +0200

    (add) Home Page

---
+++ Shop/Shop/Shop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shop.Interfaces;
+using Shop.ViewModels;
+
+namespace Shop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICarRepository _carRepository;
+
+        public HomeController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                Cars = _carRepository.Cars
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ Shop/Shop/Shop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Car> Cars { get; set; }
+
+    }
+}
